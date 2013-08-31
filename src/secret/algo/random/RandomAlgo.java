1f926cfa4193210faa0bab7ebe8a78843a985ba8
Jack Qiu <luo.fish@hotmail.com>
2013-09-01 02:25:40 +0800

    2013-09-01 Add dynamic random algorithm Add basic layout of buttons and textView to display record

--- src/secret/algo/random/RandomAlgo.java
+++ src/secret/algo/random/RandomAlgo.java
@@ -1,10 +1,32 @@
 package secret.algo.random;
 
+import java.util.*;
+
 public class RandomAlgo {
-	
-	
-	public void getRandom() {
-		
+
+	public char[] getRandomBinary(int scopeMax) {
+		ArrayList<Float> randomFloatList = new ArrayList<Float>();
+		randomFloatList = (ArrayList<Float>) getRandomFloatList(scopeMax - 1);
+
+		float seed = randomFloatList.get(0);
+
+		Collections.sort(randomFloatList);
+
+		int randomNumber = randomFloatList.indexOf(seed);
+		char[] randomBinaryNumberArray = Integer.toBinaryString(randomNumber)
+				.toCharArray();
+
+		return randomBinaryNumberArray;
 	}
 
+	private List<Float> getRandomFloatList(int listScope) {
+		Random random = new Random();
+		List<Float> randomFloatList = new ArrayList<Float>();
+
+		for (int i = 0; i < listScope;) {
+			randomFloatList.add(random.nextFloat());
+			i++;
+		}
+		return randomFloatList;
+	}
 }
--- src/secret/app/baccarat/MainActivity.java
+++ src/secret/app/baccarat/MainActivity.java
@@ -3,15 +3,60 @@ package secret.app.baccarat;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private Button bankerButton;
+	private Button playerButton;
+	private Button drawButton;
+
+	private TextView betCount;
+	private TextView last3rdRecord;
+	private TextView last2ndRecord;
+	private TextView last1stRecord;
+	private TextView storedBinaryRecord;
+	
+	private final int bankerButtonId = R.id.buttonBetBanker;
+	private final int playerButtonId = R.id.buttonBetPlayer;
+	private final int drawButtonId = R.id.buttonBetDraw;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		displayedWedgetInit();
 	}
 
+	private void displayedWedgetInit() {
+
+		bankerButton = (Button) findViewById(bankerButtonId);
+		bankerButton.setOnClickListener(buttonListener);
+		playerButton = (Button) findViewById(playerButtonId);
+		playerButton.setOnClickListener(buttonListener);
+		drawButton = (Button) findViewById(drawButtonId);
+		drawButton.setOnClickListener(buttonListener);
+
+		betCount = (TextView) findViewById(R.id.textViewBetCount);
+		last1stRecord = (TextView) findViewById(R.id.textViewLast1stRecord);
+		last2ndRecord = (TextView) findViewById(R.id.textViewLast3rdRecord);
+		storedBinaryRecord = (TextView) findViewById(R.id.textViewStoredBinaryArray);
+	}
+	
+	private Button.OnClickListener buttonListener = new Button.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			updateDisplayBetData(v.getId());
+		}
+	}; 
+
+	private void updateDisplayBetData(int id) {
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
